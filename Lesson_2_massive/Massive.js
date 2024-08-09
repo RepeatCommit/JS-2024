644268b446f4d2851d73f78276e92f95bdf4bf9b
syomagit <shulyakovskiy97@gmail.com>
2024-08-09 23:24:45 +0300

    create new

---
+++ Lesson_2_massive/Massive.js
@@ -0,0 +1,238 @@
+// let array = [189,'blank',true,false,66,'some','container',434,123,'last'];
+// console.log(array[0]);
+// console.log(array[1]);
+// console.log(array[2]);
+// console.log(array[3]);
+// console.log(array[4]);
+// console.log(array[5]);
+// console.log(array[6]);
+// console.log(array[7]);
+// console.log(array[8]);
+// console.log(array[9]);
+
+let book = {
+    title: 'main',
+    pageCount: 3,
+    genre: 'scary',
+    authors: [],
+}
+let book1 = {
+    title: 'main',
+    pageCount: 3,
+    genre: 'comedy',
+    authors: [],
+}
+let book2 = {
+    title: 'main',
+    pageCount: 3,
+    genre: 'adventure',
+    authors: [],
+}
+book.authors.name = 'King';
+book1.authors.name = 'Hitch';
+book2.authors.name = 'Caroll';
+
+book.authors.age = 75;
+book1.authors.age = 56;
+book2.authors.age = 45;
+// я вивів таким чином name та age.Цікаво чи це правильно?
+console.log(book,book1,book2);
+
+// let users = [
+//     {name:'Samsom',username:'Som',password:123},
+//     {name:'Karlson',username:'Son',password:321},
+//     {name:'Mamon',username:'Mon',password:456},
+//     {name:'Drake',username:'Ake',password:789},
+//     {name:'Linch',username:'Inch',password:333},
+//     {name:'Tom',username:'Mot',password:222},
+//     {name:'Michael',username:'Chael',password:444},
+//     {name:'Kyran',username:'Ran',password:999},
+//     {name:'Anna',username:'Nna',password:998},
+//     {name:'Ira',username:'Ra',password:676},
+// ]
+// console.log(users);
+// console.log(users[0].password);
+// console.log(users[1].password);
+// console.log(users[2].password);
+// console.log(users[3].password);
+// console.log(users[4].password);
+// console.log(users[5].password);
+// console.log(users[6].password);
+// console.log(users[7].password);
+// console.log(users[8].password);
+// console.log(users[users.length - 1].password);
+//
+// let weather = [
+//     {morning: 5, afternoon: 20, evening: 12},
+//     {morning: 6, afternoon: 22, evening: 14},
+//     {morning: 7, afternoon: 21, evening: 13},
+//     {morning: 5, afternoon: 23, evening: 14},
+//     {morning: 8, afternoon: 22, evening: 12},
+//     {morning: 7, afternoon: 25, evening: 16},
+//     {morning: 8, afternoon: 21, evening: 13},
+// ]
+// console.log(weather);
+
+// let x;
+// if (x !== 0) {
+//     console.log('good')
+//
+// }else{
+//     console.log('bad');
+// }
+// let x = 1;
+// if (x === 0) {
+//     console.log('good')
+//
+// }else{
+//     console.log('bad');
+//  }
+// let a = -3;
+// if (a === -3){
+//     console.log('best')
+// } else{
+//     console.log('worst')
+// }
+
+// let time = 32;
+// if(time>= 0 && time <=15) {
+//     console.log('First');
+// } else if (time>= 16 && time <= 30) {
+//     console.log('Second') ;
+// } else if (time>= 31 && time <= 45) {
+//     console.log('Third') ;
+// } else if (time>= 46 && time <= 59){
+//         console.log('Four') ;
+// }
+
+// let day = 8;
+// if(day>= 1 && day <=10){
+//     console.log('Decadafirst');
+// }
+// else if (day>= 11 && day <=20){
+//     console.log('Decadasecond') ;
+// }
+// else if (day>= 21 && day <=31){
+//     console.log('Decadathird');
+// }
+
+// let schedule = 'wednesday'
+// switch (schedule) {
+//     case 'monday':
+//         console.log('gym')
+//         break;
+//     case 'tuesday':
+//         console.log('work')
+//         break;
+//     case 'friday':
+//         console.log('rest')
+//         break;
+//     case 'wednesday':
+//         console.log('fishing')
+//         break;
+//     case 'thursday':
+//         console.log('shopping')
+//         break;
+// }
+
+
+
+// let a = 20;
+// let b = 20;
+// if (a > b) {
+//     console.log(a)
+// }
+//  else if (a < b){
+//     console.log(b);
+// }
+//  else if(a === b){
+//     console.log('the same');
+// }
+
+// let  x = 2;
+// x = 0 || 'default'
+// console.log(x)
+
+// let x = 5
+// if (x === 0){
+//     console.log(x)
+// }else{
+//     console.log('default')
+// }
+
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+// if (coursesAndDurationArray[0].monthDuration > 5){
+//     console.log('super');
+// }
+// if (coursesAndDurationArray[1].monthDuration > 5){
+//     console.log('super');
+// }
+// if (coursesAndDurationArray[2].monthDuration > 5){
+//     console.log('super');
+// }
+// if (coursesAndDurationArray[3].monthDuration > 5){
+//     console.log('super');
+// }
+// if (coursesAndDurationArray[4].monthDuration > 5){
+//     console.log('super');
+// }
+// if (coursesAndDurationArray[5].monthDuration > 5){
+//     console.log('super');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
